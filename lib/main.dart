dc5b5c13e75c37d41f326dc8cedda9dd79548d4f
ArgyaAziz <argyaagi98@gmail.com>
2023-11-14 10:52:48 +0700

    membuat database

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:ch13_local_persistence/pages/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Local Persistence',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        // ignore: deprecated_member_use
+        bottomAppBarColor: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
